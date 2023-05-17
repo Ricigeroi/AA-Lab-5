e2a226f6d97b372dffb5764fe01d4938a9d35658
Ricigeroi <andrey.bardier@gmail.com>
2023-05-18 02:52:05 +0300

    meow

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def run(n):
 
     print("SPARSE GRAPH, n =", n)
     print("Dijkstra time:", d1)
-    print("Floyd-marshall time:", f1)
+    print("Floyd-Warshall time:", f1)
 
     d2 = dijkstra(dense_graph)
     f2 = floyd(dense_graph)
@@ -67,11 +67,56 @@ def run(n):
     print("\n")
     print("DENSE GRAPH, n =", n)
     print("Dijkstra time:", d2)
-    print("Floyd-marshall time:", f2)
+    print("Floyd-Warshall time:", f2)
 
     return d1, f1, d2, f2
 
 
-print(run(500))
+n = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600]
 
+dijkstra_time_sparse = []
+floyd_time_sparse = []
+
+dijkstra_time_dense = []
+floyd_time_dense = []
+
+
+for i in n:
+    result = run(i)
+
+    dijkstra_time_sparse.append(result[0])
+    floyd_time_sparse.append(result[1])
+
+    dijkstra_time_dense.append(result[2])
+    floyd_time_dense.append(result[3])
+
+print("\n\n")
+print(dijkstra_time_sparse)
+print(floyd_time_sparse)
+print("\n")
+print(dijkstra_time_dense)
+print(floyd_time_dense)
+
+
+# Plotting sparse results
+plt.figure(figsize=(8, 6))
+plt.plot(n, dijkstra_time_sparse, label='Dijkstra')
+plt.plot(n, floyd_time_sparse, label='Floyd-Warshall')
+plt.xlabel('Number of vertices')
+plt.ylabel('Time')
+plt.title('Sparse Graph Results')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Plotting dense results
+plt.figure(figsize=(8, 6))
+plt.plot(n, dijkstra_time_dense, label='Dijkstra')
+plt.plot(n, floyd_time_dense, label='Floyd-Warshall')
+plt.xlabel('Number of vertices')
+plt.ylabel('Time')
+plt.title('Dense Graph Results')
+plt.legend()
+plt.grid(True)
+plt.show()
 
